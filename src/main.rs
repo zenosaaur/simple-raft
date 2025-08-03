297e7ae3d2088f591703d60f1ba3b4c52eae81f8
Davide Zeni <davidezeni02@gmail.com>
2025-08-03 13:49:29 +0200

    setup grpc

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use proto::calculator_server::{Calculator, CalculatorServer};
+use tonic::transport::Server;
+
+mod proto {
+    tonic::include_proto!("calculator");
+
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("calculator_descriptor");
+}
+
+#[derive(Debug, Default)]
+struct CalculatorService {}
+
+#[tonic::async_trait]
+impl Calculator for CalculatorService {
+    async fn add(
+        &self,
+        request: tonic::Request<proto::CalculationRequest>,
+    ) -> Result<tonic::Response<proto::CalculationReply>, tonic::Status> {
+        println!("Got a request {:?}", request);
+
+        let input = request.get_ref();
+
+        let response = proto::CalculationReply {
+            result: input.a + input.b,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+
+    let calc = CalculatorService::default();
+
+    Server::builder()
+        .add_service(CalculatorServer::new(calc))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
